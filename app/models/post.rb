04b60d4e83fd6ffd80e6646d18fc5f0ff33c061d
yuki <yymmt3.waste@gmail.com>
2018-05-18 15:49:34 +0900

    Merge commit 'c47c2de'

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
     validates :body, presence: true
     # validates :name, presence: true, uniqueness: true
 
-    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    # has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+    # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
     friendly_id :title, use: :slugged
 
